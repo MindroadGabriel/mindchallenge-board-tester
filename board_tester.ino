14ae34df51ba0baa792251bd67b3b52c79b47b5f
Gabriel Nilsson <gabriel.nilsson@mindroad.se>
2025-02-04 16:57:15 +0100

    Combine with display

--- board_tester.ino
+++ board_tester.ino
@@ -1,3 +1,6 @@
+#include <Adafruit_SSD1306.h>
+#include <splash.h>
+
 #include <Wire.h>
 #include <inttypes.h>
 
@@ -9,7 +12,17 @@ uint8_t AHTX0_STATUS_BUSY = 0x80;  // Status bit for busy
 uint8_t AHTX0_STATUS_CALIBRATED = 0x08;  // Status bit for calibrated
 
 
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+#define LOGO_HEIGHT   16
+#define LOGO_WIDTH    16
+
 void transmit(uint8_t address, uint8_t buffer[], uint8_t buffer_size) {
+  delay(10); // This device needs some space after other I2C devices are done talking
   Wire.beginTransmission(address);
   uint8_t write_result = Wire.write(buffer, buffer_size);
   if (write_result != buffer_size) {
@@ -23,19 +36,14 @@ void transmit(uint8_t address, uint8_t buffer[], uint8_t buffer_size) {
   }
 }
 uint8_t receive(uint8_t address, uint8_t out_buffer[], uint8_t buffer_size) {
+  for (int i = 0; i < buffer_size; ++i) {
+    out_buffer[i] = 0x00;
+  }
   Wire.requestFrom(address, (uint8_t)buffer_size);
   for (int i = 0; i < buffer_size; ++i) {
     if (Wire.available()) {
       out_buffer[i] = Wire.read();
-      /*
-      Serial.write("Read ");
-      Serial.print(out_buffer[i]);
-      Serial.write("\n");
-      // */
     } else {
-      //Serial.write("Too few bytesp available. Read ");
-      //Serial.print(i);
-      //Serial.write("\n");
       return i;
     }
   }
@@ -86,55 +94,62 @@ void calibrate(uint8_t address) {
     Serial.print("Calibrated\n");
   }
 }
-void read_data(uint8_t address, float &out_humidity, float &out_temperature) {
+void read_data(uint8_t address, float &out_humidity, float &out_temperature, bool print) {
   transmit(address, AHTX0_CMD_TRIGGER, 3);
-  
+
   int attempts = 0;
-  uint8_t status = get_status(address, true);
+  uint8_t status = get_status(address, false);
   while ((status & AHTX0_STATUS_BUSY) != 0 && attempts < 100) {
-    delay(20);
-    status = get_status(address, false);
+    delay(2000);
+    status = get_status(address, print);
   }
   if ((status & AHTX0_STATUS_BUSY) != 0) {
-    Serial.print("Failed to generate new data.\n");
+    if (print) {
+      Serial.print("Failed to generate new data.\n");
+    }
     return;
   }
-  const uint8_t buffer_length = 6;
-  uint8_t buffer[buffer_length];
-  uint8_t received_length = receive(address, buffer, buffer_length);
-  if (received_length != buffer_length) {
+  const uint8_t buffer_size = 6;
+  uint8_t buffer[buffer_size];
+  uint8_t received_length = receive(address, buffer, buffer_size);
+  if (received_length != buffer_size) {
     Serial.print("Failed to read all bytes when reading data. Read ");
     Serial.print(received_length);
     Serial.print("/");
-    Serial.print(buffer_length);
+    Serial.print(buffer_size);
     Serial.print(" bytes.\n");
   }
-  Serial.print("Raw data: ");
-  for (int i = 0; i < buffer_length; ++i) {
-    Serial.print((uint8_t)buffer[i], 16);
+  if (print) {
+    Serial.print("Raw data: ");
+    for (int i = 0; i < buffer_size; ++i) {
+      Serial.print((uint8_t)buffer[i], 16);
+    }
+    Serial.print(", ");
+
   }
-  Serial.print("\n");
   uint32_t raw_humidity = (((uint32_t)buffer[1]) << 12) |
     (((uint32_t)buffer[2]) << 4) |
     (((uint32_t)buffer[3]) >> 4);
   uint32_t raw_temperature = ((((uint32_t)buffer[3]) & 0xF) << 16) |
     (((uint32_t)buffer[4]) << 8) |
     ((uint32_t)buffer[5]);
-  Serial.print("Raw humidity: ");
-  Serial.print(raw_humidity, 16);
-  Serial.print(", raw temperature: ");
-  Serial.print(raw_temperature, 16);
-  Serial.print("\n");
+  if (print) {
+    Serial.print("raw humidity: ");
+    Serial.print(raw_humidity, 16);
+    Serial.print(", raw temperature: ");
+    Serial.print(raw_temperature, 16);
+    Serial.print("\n");
+  }
 
   out_humidity = (((float)raw_humidity) * 100) / 0x100000;
   out_temperature = ((((float)raw_temperature) * 200) / 0x100000) - 50;
 }
 
 void setup() {
-  //digitalWrite(SCL, 1);
   delay(20);
   Serial.begin(9600);
   Serial.write("--- Restart ---\n");
+  
   Wire.begin();
   uint8_t address = AHTX0_I2CADDR_DEFAULT;
   Wire.begin(address);
@@ -145,42 +160,61 @@ void setup() {
     delay(2000);
   }
   calibrate(address);
-  float humidity = 0.0f;
-  float temperature = 0.0f;
-  read_data(address, humidity, temperature);
-  Serial.print("Humidity: ");
-  Serial.print(humidity);
-  Serial.print("%, temperature: ");
-  Serial.print(temperature);
-  Serial.print(" degrees Celcius\n");
+  // */
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }// */
+  display.clearDisplay();
+  display.display();
+}
+
+void writeString(char* text) {
+  char* c = text;
+  while(*c) {
+    display.write(*c);
+    c++;
+  }
+}
 
 
-  //send_reset(address);
-  /*
-  uint8_t status = get_status(address);
-  Serial.print("Status is: 0x");
-  Serial.print(status, 16);
-  Serial.println();*/
-  /*
-  if (status != 0xFF) {
-    Serial.write("Waiting for temperature and humidity sensor (AHT10) to start\n");
-    int attempts = 0;
-    while (status != 0xFF && attempts < 1000) {
-      attempts += 1;
-      delay(20);
-      status = get_status(address);
-    }
-    if (status != 0xFF) {
-      Serial.write("AHT10 failed to start.\n");
-    } else {
-      Serial.write("AHT10 started!\n");
-    }
-  } 
-  calibrate(address);
-*/
+void writeFloatString(float n) {
+  char buffer[128];
+  dtostrf(n, 4, 2, buffer);
+  writeString(buffer);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  display.clearDisplay();
+
+  display.setTextSize(1);      // Normal 1:1 pixel scale
+  display.setTextColor(WHITE); // Draw white text
+  display.setCursor(0, 0);     // Start at top-left corner
+  display.cp437(true);         // Use full 256 char 'Code Page 437' font
+  display.display();
+  while (true) {
+    uint8_t address = AHTX0_I2CADDR_DEFAULT;
+    float humidity = 0.0f;
+    float temperature = 0.0f;
+    read_data(address, humidity, temperature, false);
+    Serial.print("Humidity: ");
+    Serial.print(humidity);
+    Serial.print("%, temperature: ");
+    Serial.print(temperature);
+    Serial.print(" degrees Celcius\n");
+    // */
+
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    writeString("\nTEMPERATURE = ");
+    writeFloatString(temperature);
+    writeString(", HUMIDITY = ");
+    writeFloatString(humidity);
+    writeString("\n");
+    display.display();
+    
+    delay(2000);
+  }
 }
